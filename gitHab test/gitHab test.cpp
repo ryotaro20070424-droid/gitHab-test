86f981cae49bd56ee486b4d99339a74acd7c2cfc
瞭太郎 八木 <y.ryotarou2007@outlook.jp>
2026-09-17 13:31:46 +0900

    かんたぁぁぁ！！！！

--- gitHab test/gitHab test.cpp
+++ gitHab test/gitHab test.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "お前んち、おっばっけやーしき！";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
